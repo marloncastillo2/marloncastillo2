e58fb4bcdbf4d4ec017f9541a1daa30ff6288e1d
marloncastillo2 <104037007+marloncastillo2@users.noreply.github.com>
2022-05-11 13:40:54 -0600

    Add files via upload

---
+++ Ejercicio1/ISNP966920_Unidad1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ISNP966920_Unidad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num =1;
+            while (num != 0)
+            {
+                Console.WriteLine("Bienvenido, ingrese por favor un valor: ");
+                num = int.Parse(Console.ReadLine());
+                if (num > 0)
+                {
+                    Console.WriteLine("El numero ingresado es positivo");
+                }
+                else
+                {
+                    Console.WriteLine("El numero ingresado es negativo");
+                }
+            }
+
+        }
+    }
+}
